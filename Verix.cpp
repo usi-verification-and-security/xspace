d51b0ef0511ebcb31f2e66c73ff2ebdda9765829
labbaf <labbaf@device-195.home>
2024-05-02 09:23:38 +0200

    init

---
+++ Verix.cpp
@@ -0,0 +1,195 @@
+//
+// Created by labbaf on 26.04.2024.
+//
+
+#include <Opensmt.h>
+#include "Verix.h"
+#include <algorithm>
+
+
+
+Verix::Verix(const NNModel& model)
+    : nn(model),
+      inputVars({"x1", "x2", "x3"}),
+      outputVars({"o1", "o2"})
+{
+
+    // this->nn = model;
+    // this->logic = opensmt::Logic_t::QF_UF;
+    // this->mainSolver = MainSolver(logic, SMTConfig(), "Verix solver");
+}
+
+void Verix::get_explanation(float epsilon, std::vector<float> input_example) {
+    /*
+     * To compute the explanation for the model and the neural network.
+     * param epsilon: the perturbation magnitude.
+     * param plot_explanation: if True, plot the explanation.
+     * param plot_counterfactual: if True, plot the counterfactual(s).
+     * param plot_timeout: if True, plot the timeout pixel(s).
+     * return: an explanation, and possible counterfactual(s).
+     */
+
+    Opensmt* osmt = pre();
+    SMTConfig& c = osmt->getConfig();
+    MainSolver& mainSolver = osmt->getMainSolver();
+    auto & logic = osmt->getLRALogic();
+
+
+    std::vector<int> unsat_set;
+    std::vector<int> sat_set;
+    std::vector<int> timeout_set;
+
+    std::vector<PTRef> outputVarsRefs;
+    std::vector<PTRef> inputVarsRefs;
+    for (int i = 0; i < inputVars.size(); i++) {
+//        name = "input_" + std::to_string(i);
+        std::string name = "input_" + std::to_string(i);
+        inputVarsRefs.push_back(logic.mkRealVar(name.c_str()));
+    }
+
+
+//    for (int i = 0; i < outputVars.size(); i++) {
+//        outputVarsRefs.push_back(logic.mkRealVar(inputVars[i].c_str()));
+//    }
+
+    /*
+     * add model encoding and create the outputs
+     * TODO: you should do this automatically in the model class
+     * */
+    // Create constants
+    PTRef five = logic.mkRealConst(FastRational(5));
+    PTRef minus_one = logic.mkRealConst(FastRational(-1));
+    PTRef minus_half = logic.mkRealConst(FastRational(-1, 2));;
+    PTRef zero = logic.mkRealConst(FastRational(0));
+
+    // Create formulas
+    PTRef h1 = logic.mkMinus(logic.mkTimes(five, inputVarsRefs[0]), logic.mkPlus(inputVarsRefs[1], inputVarsRefs[2]));
+    PTRef h2 = logic.mkPlus(vec<PTRef>{logic.mkNeg(inputVarsRefs[0]), inputVarsRefs[1], inputVarsRefs[2]});
+
+    // Create ReLU function
+    PTRef h1_relu = logic.mkIte(logic.mkGt(h1, zero), h1, zero);
+    PTRef h2_relu = logic.mkIte(logic.mkGt(h2, zero), h2, zero);
+
+    // Create output formulas
+    PTRef o1 = logic.mkMinus(h1_relu, h2_relu);
+    PTRef o2 = logic.mkPlus(logic.mkTimes(minus_half, h1_relu), h2_relu);
+    outputVarsRefs.push_back(o1);
+    outputVarsRefs.push_back(o2);
+
+//    {
+//        for (int i = 0; i < input_example.size(); ++i) {
+////                set lower bound for the variable
+//                FastRational lower_bound_fr(input_example[i], 1); // Create FastRational from float
+////                set upper bound for the variable
+//                FastRational upper_bound_fr(input_example[i], 1); // Create FastRational from float
+//                mainSolver.insertFormula(logic.mkAnd(logic.mkLeq(inputVarsRefs[i], logic.mkRealConst(lower_bound_fr)),
+//                                                     logic.mkLeq(logic.mkRealConst(upper_bound_fr), inputVarsRefs[i])));
+//        }
+//        mainSolver.insertFormula(logic.mkLt(outputVarsRefs[0], outputVarsRefs[1]));
+//        auto res = mainSolver.check();
+//        assert(res == s_False);
+//        auto itp = mainSolver.getInterpolationContext();
+//        vec<PTRef> itps;
+//        ipartitions_t partitions = 1;
+//        itp->getSingleInterpolant(itps, partitions);
+//        assert(itps.size() == 1);
+//        std::cout << logic.pp(itps[0]);
+//        exit(1);
+//    }
+
+    std::vector<float> output_vars = nn.predict(input_example);
+    auto maxElementIter = std::max_element(output_vars.begin(), output_vars.end());
+    int label = std::distance(output_vars.begin(), maxElementIter);
+
+    float lower_bound;
+    float upper_bound;
+    for (int feature = 0; feature < input_example.size(); feature++) {
+        mainSolver.push();
+        for (int i = 0; i < input_example.size(); i++) {
+            /*
+             * Set constraints on the input variables.
+             */
+
+            if (i == feature || std::find(unsat_set.begin(), unsat_set.end(), i) != unsat_set.end()) {
+                /*
+                 * Set allowable perturbations on the current feature and the irrelevant features.
+                 */
+                lower_bound =  std::max(input_example[i] - epsilon, 0.0f);
+                upper_bound =  std::min(input_example[i] + epsilon, 1.0f);
+//                set lower bound for the variable
+                FastRational lower_bound_fr(lower_bound, 1); // Create FastRational from float
+                mainSolver.insertFormula(logic.mkLeq( logic.mkRealConst(lower_bound_fr), inputVarsRefs[i]));
+//                set upper bound for the variable
+                FastRational upper_bound_fr(upper_bound, 1); // Create FastRational from float
+                mainSolver.insertFormula(logic.mkLeq(inputVarsRefs[i], logic.mkRealConst(upper_bound_fr)));
+            } else {
+                /*
+                 * Make sure the other pixels are fixed.
+                 */
+                lower_bound =  input_example[i];
+                upper_bound =  input_example[i];
+//                set lower bound for the variable
+                FastRational lower_bound_fr(lower_bound, 1); // Create FastRational from float
+                mainSolver.insertFormula(logic.mkLeq(inputVarsRefs[i], logic.mkRealConst(lower_bound_fr)));
+//                set upper bound for the variable
+                FastRational upper_bound_fr(upper_bound, 1); // Create FastRational from float
+                mainSolver.insertFormula(logic.mkLeq(logic.mkRealConst(upper_bound_fr), inputVarsRefs[i]));
+            }
+        }
+
+
+        for (int j = 0; j < output_vars.size(); j++) {
+            /*
+             * Set constraints on the output variables.
+             */
+            if (j != label) {
+                // [1, -1], -1e-6,
+                // isProperty=True)
+                vec<PTRef> args_lt;
+                args_lt.push(outputVarsRefs[label]);
+                args_lt.push(outputVarsRefs[j]);
+                PTRef formula = logic.mkLt(args_lt);
+                mainSolver.insertFormula(formula);
+
+            }
+        }
+
+        sstat r = mainSolver.check();
+        if (r == s_True) {
+            printf("sat\n");
+            sat_set.push_back(feature);
+            auto m = mainSolver.getModel();
+            std::vector<float> cex;
+            for (int i = 0; i < inputVarsRefs.size(); i++) {
+                auto v1_p = logic.pp(m->evaluate(inputVarsRefs[i]));
+                auto var_a_s = logic.pp(inputVarsRefs[i]);
+                printf("%s, ", v1_p.c_str());
+            }
+            printf("\n");
+            const std::vector<float> &prediction = nn.predict(input_example);
+//            print the prediction
+//            for (int i = 0; i < prediction.size(); i++) {
+//                printf("%f ", prediction[i]);
+//            }
+//            printf("\n");
+
+
+        } else if (r == s_False) {
+            printf("unsat\n");
+            unsat_set.push_back(feature);
+        } else if (r == s_Undef) {
+            printf("unknown\n");
+            timeout_set.push_back(feature);
+        } else {
+            printf("error\n");
+        }
+        mainSolver.pop();
+    }
+
+//    print sat_set
+    printf("Explanation:  \n");
+    for (int i = 0; i < sat_set.size(); i++) {
+        printf("%d ", sat_set[i]);
+    }
+    printf("\n");
+}
---
+++ Verix.h
@@ -0,0 +1,115 @@
+//
+// Created by labbaf on 26.04.2024.
+//
+
+#ifndef XAI_SMT_VERIX_H
+#define XAI_SMT_VERIX_H
+
+
+#include "NNModel.h"
+#include <Opensmt.h>
+#include <vector>
+
+class Verix {
+    private:
+    NNModel nn;
+//    Logic logic;
+//    SMTConfig config;
+//    MainSolver mainSolver;
+    std::vector<std::string> inputVars;
+    std::vector<std::string> outputVars;
+
+    public:
+    Verix(const NNModel& model);
+    void get_explanation(float epsilon, std::vector<float> input_example);
+
+    Opensmt*
+    pre()
+    {
+        auto config = std::make_unique<SMTConfig>();
+        const char* msg;
+        config->setOption(SMTConfig::o_produce_inter, SMTOption(true), msg);
+        Opensmt* osmt = new Opensmt(opensmt_logic::qf_lra, "test solver", std::move(config));
+        return osmt;
+    }
+
+};
+//
+//def get_explanation(self, epsilon):
+//    """
+//    To compute the explanation for the model and the neural network.
+//    :param epsilon: the perturbation magnitude.
+//    :param plot_explanation: if True, plot the explanation.
+//    :param plot_counterfactual: if True, plot the counterfactual(s).
+//    :param plot_timeout: if True, plot the timeout pixel(s).
+//    :return: an explanation, and possible counterfactual(s).
+//    """
+//    unsat_set = []
+//    sat_set = []
+//    timeout_set = []
+//    input_example = self.input_example
+//    for feature in self.inputVars:
+//    for i in self.inputVars:
+//            """
+//            Set constraints on the input variables.
+//            """
+//            if i == feature or i in unsat_set:
+//            """
+//            Set allowable perturbations on the current feature and the irrelevant features.
+//            """
+//            self.mara_model.setLowerBound(i, max(0, input_example[i] - epsilon))
+//            self.mara_model.setUpperBound(i, min(1, input_example[i] + epsilon))
+//
+//        else:
+//        """
+//        Make sure the other pixels are fixed.
+//        """
+//        self.mara_model.setLowerBound(i, input_example[i])
+//        self.mara_model.setUpperBound(i, input_example[i])
+//
+//        for j in range(len(self.outputVars)):
+//        """
+//        Set constraints on the output variables.
+//    """
+//    if j != self.label:
+//    self.mara_model.addInequality([self.outputVars[self.label], self.outputVars[j]],
+//    [1, -1], -1e-6,
+//    isProperty=True)
+//    exit_code, vals, stats = self.mara_model.solve(verbose=True)
+//    """
+//    additionalEquList.clear() is to clear the output constraints.
+//    """
+//    self.mara_model.additionalEquList.clear()
+//    if exit_code == 'sat' or exit_code == 'TIMEOUT':
+//    break
+//    elif exit_code == 'unsat':
+//    continue
+//    """
+//    clearProperty() is to clear both input and output constraints.
+//    """
+//    self.mara_model.clearProperty()
+//    """
+//    If unsat, put the pixel into the irrelevant set;
+//    if timeout, into the timeout set;
+//    if sat, into the explanation.
+//    """
+//    if exit_code == 'unsat':
+//    unsat_set.append(feature)
+//    elif exit_code == 'TIMEOUT':
+//    timeout_set.append(feature)
+//    elif exit_code == 'sat':
+//    sat_set.append(feature)
+//
+//    counterfactual = [vals.get(i) for i in self.mara_model.inputVars[0].flatten()]
+//    output = [vals.get(i) for i in self.outputVars]
+//    prediction = np.asarray(output).argmax()
+//    print("Counterfactual for input ", feature, " : ", counterfactual)
+//    print("Output: ", output)
+//    print("Prediction: ", prediction)
+//
+//    explanation = [self.input_example[i] for i in sat_set]
+//    print("Explanation: ", sat_set)
+//
+//
+//
+#endif //XAI_SMT_VERIX_H
