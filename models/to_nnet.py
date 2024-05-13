971d97157641555aafc3bcc5320f2f338b09df3d
labbaf <labbaf@labbafs-MBP.usilu.net>
2024-05-13 20:52:47 +0200

    cleanup

--- models/to_nnet.py
+++ models/to_nnet.py
@@ -1,45 +1,38 @@
-import h5py
-import numpy as np
-import json
-from tensorflow.keras.models import load_model
 
-import numpy as np
+from keras.src.saving import load_model
+from NNet.utils.writeNNet import writeNNet
 
-def convert_to_nnet(model, nnet_file):
-    # Extract the necessary information from the model
-    num_layers = len(model.layers)
+
+def keras2nnets(kerasFile, input_min=-1000000, input_max=1000000, custom_objects=None):
+    # Load the model
+    model = load_model(kerasFile, custom_objects=custom_objects)
+
+    # Get a list of the model weights
+    model_params = model.get_weights()
+
+    # Split the network parameters into weights and biases, assuming they alternate
+    weights = model_params[0:len(model_params):2]
+    biases = model_params[1:len(model_params):2]
+
+    # Transpose weight matrices
+    weights = [w.T for w in weights]
+
     num_inputs = model.input_shape[1]
     num_outputs = model.output_shape[1]
-    max_layer_size = 40
 
-    # Write the header and network architecture information
-    with open(nnet_file, 'w') as f:
-        f.write("// Neural Network File Format\n")
-        f.write(f"{num_layers},{num_inputs},{num_outputs},{max_layer_size},\n")
+    # Min and max values used to bound the inputs
+    input_mins = [input_min for _ in range(num_inputs)]
+    input_maxes = [input_max for _ in range(num_inputs)]
 
-        # Write the layer sizes
-        layer_sizes = [num_inputs] + [layer.units for layer in model.layers]
-        f.write(','.join(map(str, layer_sizes)) + ',\n')
+    # Mean and range values for normalizing the inputs and outputs. All outputs are normalized with the same value
+    means = [0 for _ in range(num_inputs+num_outputs)]
+    ranges = [1 for _ in range(num_inputs+num_outputs)]
 
-        # Write the unused flag
-        f.write("0,\n")
+    # Tensorflow pb file to convert to .nnet file
+    nnetFile = kerasFile[:-2] + 'nnet'
 
-        # Write the minimum and maximum values of inputs
-        f.write("-100000.0," * num_inputs + "\n")
-        f.write("100000.0," * num_inputs + "\n")
-
-        # Write the mean and range values of inputs and outputs for normalization
-        f.write("0.0," * (num_inputs + 1) + "\n")
-        f.write("1.0," * (num_inputs + 1) + "\n")
-
-        # Write the weights and biases for each layer
-        for layer in model.layers:
-            weights, biases = layer.get_weights()
-            for w in weights:
-                f.write(','.join(map(str, w)) + ',\n')
-            f.write(','.join(map(str, biases)) + ',\n')
+    # Convert the file
+    writeNNet(weights, biases, input_mins, input_maxes, means, ranges, nnetFile)
 
 
-# Usage
-model = load_model('heartAttack_1hidden.h5')
-convert_to_nnet(model, 'heartAttack_1hidden.nnet')
+keras2nnets('heartAttack.h5')
