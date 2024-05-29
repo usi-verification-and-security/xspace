7d18afdaf2944b186cc762f4e28b30542d38c81c
labbaf <labbaf@labbafs-MacBook-Pro.local>
2024-05-29 17:51:52 +0200

    add model output to data file

---
+++ experiments/VerixExperiments.cpp
@@ -0,0 +1,83 @@
+//
+// Created by labbaf on 29.05.2024.
+//
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "VerixExperiments.h"
+#include "algorithms/BasicVerix.h"
+#include "verifiers/opensmt/OpenSMTVerifier.h"
+#include "verifiers/marabou/MarabouVerifier.h"
+
+void
+VerixExperiments::experiment_on_dataset(std::string modelPath, std::string datasetPath, std::string verifier,
+                                        std::string outputPath, int featureSize, float freedom_factor=0.5) {
+    xai::algo::BasicVerix algo(modelPath);
+    if (verifier == "OpenSMT")
+        algo.setVerifier(std::make_unique<xai::verifiers::OpenSMTVerifier>());
+    else if (verifier == "Marabou")
+        algo.setVerifier(std::make_unique<xai::verifiers::MarabouVerifier>());
+
+    // Open the data CSV file
+    std::ifstream file(datasetPath);
+    if (!file.is_open()) {
+        std::cout << "Could not open file " << datasetPath << std::endl;
+        return;
+    }
+
+    //open the output CSV file
+    std::ofstream outputFile(outputPath);
+    if (!outputFile.is_open()) {
+        std::cout << "Could not open file " << outputPath << std::endl;
+        return;
+    }
+
+    std::vector<std::vector<float>> data;
+    std::string line;
+    std::string header;
+    // Read the first line to skip the header
+
+    getline(file, header);
+    outputFile << "datapoint" << "," << header;
+    // Iterate over each line in the file
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string field;
+        std::vector<float> row;
+        std::vector<float> datapoint;
+        std::cout << "row: ";
+        std::cout << line;
+        while (std::getline(ss, field, ',')) {
+            // Convert the field to float and add it to the row vector
+            row.push_back(std::stof(field));
+        }
+        float output = row[featureSize];
+        datapoint = std::vector<float>(row.begin(), row.begin() + featureSize);
+        data.push_back(datapoint);
+
+        auto res = algo.computeExplanation(datapoint, 0.5);
+        std::cout <<"explanation: ";
+        std::vector<int> explanation(featureSize, 0);
+        for (auto val : res.explanation) {
+            std::cout << val << " ";
+            if (val >= 0 && val < featureSize) {
+                explanation[val] = 1;
+            }
+        }
+        std::cout << std::endl;
+
+        // add results to output file
+        for (int i = 0; i < datapoint.size(); ++i) {
+            outputFile << datapoint[i] << " ";
+        }
+        outputFile << ",";
+        for (int i = 0; i < explanation.size(); ++i) {
+            outputFile << explanation[i];
+            outputFile << ",";
+        }
+        outputFile << output << "," << "\n";
+    }
+
+    // Close the file
+    file.close();
+}
--- main.cpp
+++ main.cpp
@@ -4,42 +4,39 @@
 #include "algorithms/BasicVerix.h"
 #include "verifiers/opensmt/OpenSMTVerifier.h"
 #include "verifiers/marabou/MarabouVerifier.h"
-//
+#include "experiments/VerixExperiments.h"
+#include <iomanip>
 //#include <stdio.h>
 
 
 int main(int argc, char* argv[])
 {
+    std::string verifier = "OpenSMT";
     std::string filename = "models/heartAttack.nnet";
-//    std::string filename = "models/dummy_network.nnet";
-//    std::string filename = "models/test_network.nnet";
+    std::string datafile = "data/heartAttack.csv";
+    float freedom_factor = 0.5;
+
+    std::ostringstream stream;
+    stream << std::fixed << std::setprecision(2) << freedom_factor;
+    std::string freedom_factor_str = stream.str();
+    std::string outputfile = "output/HA_" + verifier + "_" + freedom_factor_str + ".csv";
+
     if (argc > 1) {
         filename = argv[1];
     }
-    xai::algo::BasicVerix algo(filename);
-    algo.setVerifier(std::make_unique<xai::verifiers::MarabouVerifier>());
-//    algo.setVerifier(std::make_unique<xai::verifiers::OpenSMTVerifier>());
-//    algo.computeExplanation({1,0,0}, 0.1);
-    auto res = algo.computeExplanation({63, 1, 3, 145, 233, 1, 0, 150, 0, 2.3, 0, 0, 1}, 1);
-//    auto res = algo.computeExplanation({1,1}, 1);
-    for (auto val : res.explanation) {
-        std::cout << val << " ";
-    }
-    std::cout << std::endl;
+//    xai::algo::BasicVerix algo(filename);
+//    algo.setVerifier(std::make_unique<xai::verifiers::MarabouVerifier>());
+
+
+//    std::vector<float> input_example = {63, 1, 3, 145, 233, 1, 0, 150, 0, 2.3, 0, 0, 1};
+//    auto res = algo.computeExplanation({1,0,0}, 0.5);
+    VerixExperiments::experiment_on_dataset(filename, datafile, verifier, outputfile, 13, freedom_factor);
+
     exit(0);
 
 
-    std::vector<float> input_example = {63, 1, 3, 145, 233, 1, 0, 150, 0, 2.3, 0, 0, 1};
-//    for(int i=0; i<13; i++){
-//        // push a random float between 0 and 1
-//        input_example.push_back((float)rand() / RAND_MAX);
-//    }
-    std::vector<float> output_example = {1.0};
-
-
-
-    Verix verix = Verix(filename, input_example, output_example);
-    verix.get_explanation(1);
+//    Verix verix = Verix(filename, input_example, output_example);
+//    verix.get_explanation(1);
 //    Logic logic{opensmt::Logic_t::QF_UF}; // UF Logic
 //    SMTConfig c;
 //    MainSolver mainSolver(logic, c, "test solver");
@@ -63,8 +60,8 @@ int main(int argc, char* argv[])
 //    else
 //        printf("error\n");
 
-    printf("Done without any serious errors\n");
+//    printf("Done without any serious errors\n");
 
-    return 0;
+//    return 0;
 }
 
