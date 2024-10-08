cfcbf26ca7e6432999687ac3d52c9bd1be04b93a
Tomáš Kolárik <tomaqa@gmail.com>
2024-10-08 17:35:24 +0200

    fixed printing of explanations

---
+++ experiments/Utils.h
@@ -0,0 +1,28 @@
+#include <string>
+#include <regex>
+#include <cassert>
+
+#include "StringConv.h"
+
+namespace xai::experiments {
+[[nodiscard]]
+inline std::string fixOpenSMTString(std::string str) {
+    static const std::regex re_minus(R"(-([0-9]+[0-9/]*))");
+    str = std::regex_replace(str.c_str(), re_minus, "(- $1)");
+
+    static const std::regex re_div(R"(([0-9]+)/([0-9]+))");
+    str = std::regex_replace(str.c_str(), re_div, "(/ $1 $2)");
+
+    return str;
+}
+
+inline std::string floatToRationalString(float value) {
+    auto s = std::to_string(value);
+    char* rationalString;
+    opensmt::stringToRational(rationalString, s.c_str());
+    std::string str{rationalString};
+    free(rationalString);
+
+    return fixOpenSMTString(std::move(str));
+}
+}
