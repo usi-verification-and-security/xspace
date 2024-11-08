6ee6bda17c0e63098bf40eb7dc02b9caf39b0321
Tomáš Kolárik <tomaqa@gmail.com>
2024-11-08 15:17:58 +0100

    customized at compile-time whether to use heart_attack or obesity dataset

--- main.cpp
+++ main.cpp
@@ -11,11 +11,18 @@
 
 using namespace xai;
 
+#define HEART_ATTACK
+
 int main(int argc, char* argv[])
 {
     std::string verifier = "OpenSMT";
+    #ifdef HEART_ATTACK
     std::string filename = "models/Heart_attack/heartAttack50.nnet";
     std::string datafile = "data/heartAttack.csv";
+    #else
+    std::string filename = "models/obesity/obesity-10-20-10.nnet";
+    std::string datafile = "data/obesity_test_data_noWeight.csv";
+    #endif
     float freedom_factor = 1;
 
     std::ostringstream stream;
@@ -26,5 +33,9 @@ int main(int argc, char* argv[])
     if (argc > 1) {
         filename = argv[1];
     }
+    #ifdef HEART_ATTACK
     experiments::VerixExperiments::experiment_on_dataset(filename, datafile, verifier, outputfile, 13, freedom_factor);
+    #else
+    experiments::VerixExperiments::experiment_on_dataset(filename, datafile, verifier, outputfile, 15, freedom_factor);
+    #endif
 }
