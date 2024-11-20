132d6677d8fd2030e72e088b394ca6d3d9044a05
Tomáš Kolárik <tomaqa@gmail.com>
2024-11-20 09:57:27 +0100

    upd. usage message

--- src/xspace/bin/main.cpp
+++ src/xspace/bin/main.cpp
@@ -10,14 +10,36 @@
 
 #include <getopt.h>
 
-void printUsage(std::ostream & os = std::cout) {
-    os << "USAGE: xspace <nn_model_fn> <dataset_fn> <verifier_name> <exp_strategies_spec> [<options>]\n"
-       << "OPTIONS:"
-       << "\n\t-h\t\tPrints this help message and exits"
-       << "\n\t-v\t\tRun in verbose mode"
-       << "\n\t-r\t\tReverse the order of variables";
-    os << std::endl;
+namespace {
+void printUsageStrategyRow(std::ostream & os, std::string_view name, std::vector<std::string_view> params = {}) {
+    os << "    " << name;
+    if (not params.empty()) {
+        os << "\tPARAMS:";
+        for (auto param : params) {
+            os << ' ' << param;
+        }
+    }
+    os << '\n';
 }
+
+void printUsageOptRow(std::ostream & os, char opt, std::string_view desc) {
+    os << "    -" << opt << "      " << desc << '\n';
+}
+
+void printUsage(std::ostream & os = std::cout) {
+    os << "USAGE: xspace <nn_model_fn> <dataset_fn> <verifier_name> <exp_strategies_spec> [<options>]\n";
+    os << "VERIFIERS: opensmt\n";
+    os << "STRATEGIES SPEC: '<spec1>[,<spec2>]...'\n";
+    os << "Each spec: '<name>[ <param>]...'\n";
+    printUsageStrategyRow(os, "abductive");
+    printUsageStrategyRow(os, "ucore", {"sample", "interval"});
+    os << "OPTIONS:\n";
+    printUsageOptRow(os, 'h', "Prints this help message and exits");
+    printUsageOptRow(os, 'v', "Run in verbose mode");
+    printUsageOptRow(os, 'r', "Reverse the order of variables");
+    os.flush();
+}
+} // namespace
 
 int main(int argc, char * argv[]) try {
     constexpr int minArgs = 4;
