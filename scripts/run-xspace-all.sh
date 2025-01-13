d5f08b17a0ede352f26764d956b5991b90eb2dad
Tomáš Kolárik <tomaqa@gmail.com>
2025-01-13 14:51:12 +0100

    provided short version of running the experiments

--- scripts/run-xspace-all.sh
+++ scripts/run-xspace-all.sh
@@ -4,6 +4,19 @@ MODELS=(models/Heart_attack/heartAttack50.nnet models/obesity/obesity-10-20-10.n
 DATASETS=(data/heartAttack.csv data/obesity_test_data_noWeight.csv)
 OUTPUT_DIRS=(output/heart_attack output/obesity)
 
+[[ -n $1 ]] && {
+    MAX_SAMPLES_SHORT=20
+    MAX_SAMPLES="$1"
+    shift
+
+    if [[ $MAX_SAMPLES == short ]]; then
+        MAX_SAMPLES=$MAX_SAMPLES_SHORT
+    elif ! [[ $MAX_SAMPLES =~ ^[1-9][0-9]*$ ]]; then
+        printf "Expected a maximum number of shuffled samples to process, got: %s\n" "$MAX_SAMPLES" >&2
+        exit 1
+    fi
+}
+
 [[ -z $CMD ]] && CMD=build/xspace
 
 ## Default for now ..
@@ -28,6 +41,15 @@ for model_idx in ${!MODELS[@]}; do
             (( $do_reverse )) && {
                 experiment+=_reverse
                 opts+=r
+            }
+
+            [[ -n $MAX_SAMPLES ]] && {
+                if (( $MAX_SAMPLES != $MAX_SAMPLES_SHORT )); then
+                    experiment+=_n$MAX_SAMPLES
+                else
+                    experiment+=_short
+                fi
+                opts+=Sn$MAX_SAMPLES
             }
 
             { time ${CMD} "$model" "$dataset" opensmt "$experiment_strategies" $opts >"${output_dir}/${experiment}.phi.txt" 2>"${output_dir}/${experiment}.stats.txt" ; } 2>"${output_dir}/${experiment}.time.txt" &
